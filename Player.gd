68b5db42f32ec995b2f12a273dea45931d71ebf4
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2024-04-22 12:12:02 -0300

    feat: prototype walls

--- Player.gd
+++ Player.gd
@@ -7,13 +7,14 @@ const GRAVITY = 10
 const START_LIST = [1.0, 2.0, 3.7, 5.0, 6.4, 7.3, 8.0, 9.7, 11.0, 12.0, 13.0]
 
 var motion = Vector2()
+var Wall = preload("res://WallNode.tscn")
+var score = 0
 
 func _ready():
 	$Timer.connect("timeout", self, "_on_timer_timeout")
 	$AnimatedSprite.play()
-	pass # Replace with function body.
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	motion.y += GRAVITY
 	if motion.y > MAXFALLSPEED:
 		motion.y = MAXFALLSPEED
@@ -27,9 +28,25 @@ func _physics_process(delta):
 	motion = move_and_slide(motion, UP)
 
 func _on_timer_timeout():
-	print('aaaa')
 	$AudioStreamPlayer2D.stop()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func Wall_reset():
+	var Wall_instance = Wall.instance()
+	Wall_instance.position = Vector2(450, rand_range(-60, 60))
+	get_parent().call_deferred("add_child", Wall_instance)
+
+func _on_Resetter_body_entered(body):
+	if body.name == "Walls":
+		body.queue_free()
+		Wall_reset()
+
+
+func _on_Detect_area_entered(area):
+	if area.name == "PointerArea":
+		score += 1
+		get_parent().get_parent().get_node("CanvasLayer/RichTextLabel").bbcode_text = "[center]Pontuação: " + str(score) + "[/center]"
+
+
+func _on_Detect_body_entered(body):
+	if body.name == "Walls":
+		get_tree().reload_current_scene()
--- Walls.gd
+++ Walls.gd
@@ -3,5 +3,5 @@ extends StaticBody2D
 func _ready():
 	pass # Replace with function body.
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	position += Vector2(-2,0)
